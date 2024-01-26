91ef7bf3669a3ae86e1c652ac80f6139cf1d8c5c
JosBautista <JosBautista@deloitte.com>
2024-01-26 12:01:49 -0600

    Uptaded

--- BasicsUtils/src/servicios/encoder.java
+++ BasicsUtils/src/servicios/encoder.java
@@ -6,37 +6,38 @@ import java.util.Base64;
 
 public class encoder {
     static Scanner scanner = new Scanner(System.in);
-    static String str = " ";
-    static String str2 = " ";
+
     static int val = 0;
     public static void encoderM() {
 
         System.out.println("1.-Encode a String to Base64.\n2.-Decode a Base64 to String\n");
-        val = scanner.nextInt();
+        String val = scanner.nextLine();
         switch (val) {
-            case 1:
-                enBase64();
+            case "1":
+                System.out.println("please enter the word to be decode: ");
+                String word = scanner.nextLine();
+                enBase64(word);
                 break;
-            case 2:
-                deBase64();
+            case "2":
+                System.out.println("Please enter the Base64 to decode to String: ");
+                String code = scanner.nextLine();
+                deBase64(code);
                 break;
             default:
                 System.out.println("Icorrect Selection");
                 encoder.encoderM();
         }
     }
-    private static void enBase64(){
-        System.out.println("please enter the word to be decode: ");
-        str = scanner.nextLine(); //i´m having issue with this, when i created just this alone is working fine, but after I create the decode is not working, is just skipping the and gets out
-        String nStr = str.trim().replace(" ", "|space-");
+    private static void enBase64(String word){
+        System.out.println(word);
+        String nStr = word.trim().replace(" ", "|space-");
         String encodeStr = Base64.getEncoder().encodeToString(nStr.getBytes());
         System.out.println(encodeStr);
     }
-    private static void deBase64(){
-        System.out.println("Please enter the Base64 to be Decode: ");
-        str2 = scanner.nextLine();
-        byte[] decodeStr = Base64.getDecoder().decode(str2); // I create a byte array to hold the string of the encode original string
+    private static void deBase64(String code){
+        byte[] decodeStr = Base64.getDecoder().decode(code); // I create a byte array to hold the string of the encode original string
         String strDecod = new String(decodeStr);
-        System.out.println(strDecod);
+        String finalStr = strDecod.trim().replace("|space-", " ");
+        System.out.println(finalStr);
     }
 }
--- BasicsUtils/src/servicios/studentsAver.java
+++ BasicsUtils/src/servicios/studentsAver.java
@@ -1,6 +1,7 @@
 package servicios;
-
+import java.util.List;
 import java.lang.String;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class studentsAver {
@@ -9,20 +10,35 @@ public class studentsAver {
     static String grade = " ";
     static int numSing = 0;
     static String signName = " ";
-    static  int score = 0;
+    static  double score = 0.0;
+    static List<String> data = new ArrayList<>();
+    static List<String> scoreVal = new ArrayList<>();
     public static void studAv(){
         System.out.println("Please enter your full name: \n");
         name = scanner.nextLine();
         System.out.println("Please enter your Grade: \n");
         grade = scanner.nextLine();
         System.out.println("Please enter the number of signatures to enroll: \n");
-        numSing = scanner.nextInt();
+        String numSing = scanner.nextLine();
 
-        for (; numSing > 0; numSing--) {
+
+        for (int numTemp = Integer.valueOf(numSing); numTemp > 0; numTemp--) {
             System.out.println("Please enter the Name of the signature: ");
-            signName = scanner.nextLine(); //I'm Stuck here, at 1:02 AM
+            signName = scanner.nextLine();
             System.out.println("Please enter the score for " + signName + " ");
-            score = scanner.nextInt();
+            String score = scanner.nextLine();
+            doct(signName, score);
+        }
+    }
+    private static void doct( String sing, List<String> valu){
+            data.add(sing);
+            int temp1 = valu.size();
+            scoreVal.add();
+    }
+    private static void evalu(){
+        int i = scoreVal.size();
+        for(; i > 0; i--){
+             int sumatory = scoreVal.get(i);
         }
     }
 }
